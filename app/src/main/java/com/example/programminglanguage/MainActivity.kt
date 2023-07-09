f325cf789e6ba50b186ac7bdde039602a6f3501f
Krisna31 <krisnaaaputraaa@gmail.com>
2023-07-09 19:59:01 +0700

    done

--- app/src/main/java/com/example/programminglanguage/MainActivity.kt
+++ app/src/main/java/com/example/programminglanguage/MainActivity.kt
@@ -1,18 +1,28 @@
 package com.example.programminglanguage
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
     private lateinit var rvLang: RecyclerView
     private val list = ArrayList<Lang>()
+    private lateinit var fabProfile: FloatingActionButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        fabProfile = findViewById(R.id.fabProfile)
+        fabProfile.setOnClickListener {
+            val intentProfile = Intent(this, ProfileActivity::class.java)
+            startActivity(intentProfile)
+        }
 
         rvLang = findViewById(R.id.rv_lang)
         rvLang.setHasFixedSize(true)
